15b6ad1b83172624c3d64f9acea0752383441d55
Piotr Haber <piotr.haber@sens.us>
2013-05-01 15:30:24 +0200

    thread termination fix

--- monitor.cpp
+++ monitor.cpp
@@ -6,10 +6,12 @@
 #include <chrono>
 #include <atomic>
 #include <mutex>
+#include <pthread.h>
+
 
 #include <tins/tins.h>
 
-#define CLEANUP_PERIOD 30
+#define CLEANUP_PERIOD 5
 #define CLEANUP_MAXAGE 4
 
 
@@ -24,8 +26,8 @@ using namespace Tins;
 #include "ZmqEventSender.h"
 
 struct CleanupFunction {
-        void cleanup_function(ClientDb *clt_db, ApDb *ap_db) {
-                while (1) {
+        void cleanup_function(ClientDb *clt_db, ApDb *ap_db, std::atomic_bool *run) {
+                while (run->load()) {
                         std::this_thread::sleep_for(std::chrono::seconds(CLEANUP_PERIOD));
                         clt_db->cleanup(CLEANUP_MAXAGE);
                         ap_db->cleanup(CLEANUP_MAXAGE);
@@ -36,6 +38,7 @@ struct CleanupFunction {
 
 CleanupFunction *cf;
 std::thread *cleanup_thread;
+std::atomic_bool *cleanup_thread_flag;
 std::once_flag terminate_flag;
 
 std::vector<Parser *> parsers;
@@ -49,7 +52,13 @@ static int ignored;
 
 void terminate_function()
 {
+        std::cerr << "Terminating, waiting for cleanup thread to finish\n";
+        cleanup_thread_flag->store(false);
+        cleanup_thread->join();
+
         delete cf;
+        delete cleanup_thread;
+        delete cleanup_thread_flag;
 
         for (std::vector<Parser *>::iterator it = parsers.begin() ; it != parsers.end(); ++it) {
                 Parser *parser = *it;
@@ -59,12 +68,11 @@ void terminate_function()
         std::cerr << "Finishing, total packets: " << count << " ignored: " << ignored << std::endl;
         std::cerr << *clientdb << std::endl;
         std::cerr << *apdb << std::endl;
-
-
         delete clientdb;
         delete apdb;
         delete sender;
 }
+
 void terminate_handler(int sig)
 {
         std::call_once(terminate_flag, terminate_function);
@@ -105,6 +113,12 @@ int main(int argc, char **argv)
 
         monitor_dev = argv[1];
         sprintf(report_dev, "tcp://%s:*", argv[2]);
+        sigset_t signal_mask;
+        sigemptyset(&signal_mask);
+        sigaddset(&signal_mask, SIGINT);
+        int rc = pthread_sigmask (SIG_BLOCK, &signal_mask, NULL);
+        if (rc != 0)
+                std::cerr << "failed to set sigmask\n";
 
         sender = new ZmqEventSender();
         if (!sender->bind(report_dev))
@@ -114,13 +128,21 @@ int main(int argc, char **argv)
         apdb = new ApDb(sender);
 
         cf = new CleanupFunction();
-        cleanup_thread = new std::thread(std::bind(&CleanupFunction::cleanup_function, std::ref(cf), clientdb, apdb));
+        cleanup_thread_flag = new std::atomic_bool();
+        cleanup_thread_flag->store(true);
+
+        cleanup_thread = new std::thread(std::bind(&CleanupFunction::cleanup_function, std::ref(cf), clientdb, apdb, cleanup_thread_flag));
 
         parsers.push_back(new RadioTapParser());
         parsers.push_back(new Dot11ApParser(apdb));
         parsers.push_back(new Dot11StaParser(clientdb, apdb));
 
         /* Ctrl-C handler */
+        sigaddset(&signal_mask, SIGINT);
+        rc = pthread_sigmask (SIG_UNBLOCK, &signal_mask, NULL);
+        if (rc != 0)
+                std::cerr << "failed to set sigmask\n";
+
         signal(SIGINT, terminate_handler);
 
         Sniffer sniffer(std::string(monitor_dev), 1500, true);
